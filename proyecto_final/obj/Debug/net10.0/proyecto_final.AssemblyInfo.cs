436d26334b4f121a9aa7ba99f88bbe9d9b93ae82
gretchen <gretchen.aburto@uamv.edu.ni>
2026-06-05 09:41:35 -0600

    Ignorar archivos temporales

--- proyecto_final/obj/Debug/net10.0/proyecto_final.AssemblyInfo.cs
+++ proyecto_final/obj/Debug/net10.0/proyecto_final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7d424a2a075a19f15d3fb6c6ba74df48769744")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
