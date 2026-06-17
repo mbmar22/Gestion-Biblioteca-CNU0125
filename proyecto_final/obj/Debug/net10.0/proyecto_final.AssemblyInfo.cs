ea513e898254e4fe0936be858a46c0662cc61c54
Maria Martinez <mbmartinezo@uamv.edu.ni>
2026-06-17 12:06:18 -0600

    Prueba 3000

--- proyecto_final/obj/Debug/net10.0/proyecto_final.AssemblyInfo.cs
+++ proyecto_final/obj/Debug/net10.0/proyecto_final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa352463890caa51bc3104ef40558db53260e18f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c098ccd8700bb264e62736c39be8e6cf5233abd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto_final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
