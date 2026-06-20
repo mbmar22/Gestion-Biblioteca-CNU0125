a3034547c9dac277f79bce5b2835665c22788657
Maria Martinez <mbmartinezo@uamv.edu.ni>
2026-06-20 16:50:08 -0600

    Funciones listas (creo???)

--- proyecto_final/05_Prestamos/FUNCIONES_PRESTAMOS.cs
+++ proyecto_final/05_Prestamos/FUNCIONES_PRESTAMOS.cs
@@ -1,20 +1,22 @@
-class Prestamos
+class PRESTAMOS
 {
     public string IdPrestamo { get; set; }
-    public REGISTRO_LIBROS.LIBROS LibroRegistrado;
-
-    public ADMINISTRACION_USUARIOS.USUARIO UsuarioRegistrado;
+    public REGISTRO_LIBROS.LIBROS Libro_Registrado { get; set; }
+    public ADMINISTRACION_USUARIOS.USUARIO Usuario_Registrado { get; set; }
     public DateTime Fecha_Prestamo { get; set; }
     public DateTime? Fecha_Devolucion { get; set; }
-    //public 
 
-    public Prestamos(
+    public PRESTAMOS(
         string idPrestamo,
+        REGISTRO_LIBROS.LIBROS libroRegistrado,
+        ADMINISTRACION_USUARIOS.USUARIO usuarioRegistrado,
         DateTime fechaPrestamo,
         DateTime? fechaDevolucion = null
     )
     {
         IdPrestamo = idPrestamo;
+        Libro_Registrado = libroRegistrado;
+        Usuario_Registrado = usuarioRegistrado;
         Fecha_Prestamo = fechaPrestamo;
         Fecha_Devolucion = fechaDevolucion;
     }
